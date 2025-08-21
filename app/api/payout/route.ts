016413d12abcfdd06ffcdde21add1c1657df9b2f
Elishaokon13 <eliscodavid@gmail.com>
2025-08-21 11:42:49 +0100

    Modified app/api/payout/route.ts

--- app/api/payout/route.ts
+++ app/api/payout/route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextRequest, NextResponse } from 'next/server';
 import { createWalletClient, http, parseEther, isAddress } from 'viem';
 import { privateKeyToAccount } from 'viem/accounts';
