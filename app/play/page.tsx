adfc3ed697eb6815a71a207e949dd8d24b9a04ac
Elishaokon13 <eliscodavid@gmail.com>
2025-08-25 08:40:25 +0100

    Modified app/play/page.tsx

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -1,9 +1,16 @@
+/* Re-export of Play tab extracted from page.tsx */
 'use client';
-
+import React from 'react';
 import Home from '../page';
+import BottomNav from '../components/BottomNav';
 
 export default function PlayPage() {
-  return <Home />;
+  return (
+    <>
+      <Home />
+      <BottomNav />
+    </>
+  );
 }
 
 
