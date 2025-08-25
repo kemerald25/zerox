1c797f8553e1fb1c7c6de3f1fea92adf901f3a5b
Elishaokon13 <eliscodavid@gmail.com>
2025-08-25 08:45:47 +0100

    Modified app/leaderboard/page.tsx

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -1,32 +1,15 @@
 'use client';
 import React from 'react';
 import BottomNav from '../components/BottomNav';
-import LeaderboardContent from '../page';
+import Home from '../page';
 
 export default function LeaderboardPage() {
   return (
     <>
-      {/* Reuse content from page.tsx where LeaderboardTab and SprintSection are exported if needed */}
-      <LeaderboardShell />
+      <Home />
       <BottomNav />
     </>
   );
-}
-
-function LeaderboardShell() {
-  // Lightweight wrapper calling the existing content components inside page.tsx via dynamic import isn't setup;
-  // For now render a small bridge by reusing the exported functions indirectly.
-  // As an MVP, we can copy the implementation into this file later if required.
-  const Dynamic = React.useMemo(() => require('../page').default, []);
-  return <Dynamic />;
-}
-
-'use client';
-
-import Home from '../page';
-
-export default function LeaderboardPage() {
-  return <Home />;
 }
 
 
