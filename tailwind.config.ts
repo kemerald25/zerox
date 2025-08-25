fa151df697fc92e329b5d37b32ec77dde48cd22f
Elishaokon13 <eliscodavid@gmail.com>
2025-08-25 12:00:11 +0100

    Modified tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Minecraft", "Minecraft Ten", "Minecraftia", "ui-sans-serif", "system-ui"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
