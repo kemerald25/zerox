6816850f6166a45c95bc699122fabb16d0d4a779
Kemerald25 <unityekeoba18@gmail.com>
2025-09-22 21:40:50 +0100

    updated

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -33,7 +33,7 @@ export async function GET() {
       webhookUrl: `${URL}/api/webhook`,
       primaryCategory: process.env.NEXT_PUBLIC_APP_PRIMARY_CATEGORY,
       tags: [],
-      heroImageUrl: process.env.NEXT_PUBLIC_APP_HERO_IMAGE,
+      heroImageUrl: "https://wordwave-pearl.vercel.app",
       tagline: process.env.NEXT_PUBLIC_APP_TAGLINE,
       ogTitle: process.env.NEXT_PUBLIC_APP_OG_TITLE,
       ogDescription: process.env.NEXT_PUBLIC_APP_OG_DESCRIPTION,
